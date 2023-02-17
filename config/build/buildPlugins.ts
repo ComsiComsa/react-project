19fbff8ec60f29e8c1687489082f82912f9cef48
ilzoto <ilzoto@rarus.ru>
2023-02-17 22:44:34 +0400

    Added navbar; added svg loader; added sidebar; added webpack HMR; added i18n

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,9 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import path from "path";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildPlugins({ paths }: BuildOptions) {
+export function buildPlugins({ paths, isDev }: BuildOptions) {
     return [
         new HtmlWebpackPlugin({
             template: paths.html
@@ -13,6 +12,10 @@ export function buildPlugins({ paths }: BuildOptions) {
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
-        })
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev)
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
